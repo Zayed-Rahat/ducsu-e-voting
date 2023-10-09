22d6267e8136bf2f280164d85323a9a0c1373d05
Md Ikhtiar Uddin <mdikhtiaruddin786@gmail.com>
2023-10-09 23:48:53 +0600

    paginations

--- administration/views.py
+++ administration/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from voting.models import Position,Voter, Candidate
 from django.contrib.auth.models import User
 from voting.forms import PositionForm, VoterForm, CandidateForm
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
 def voters_home(request):
      return render(request, 'voters_home.html')
@@ -15,12 +16,25 @@ def dashboard(request):
 
 
 # all position showing here
+
+
 def position(request):
     if request.user.username == 'admin':
-      positions = Position.objects.all() # all position assign to positons for showing
-      return render(request, 'position.html', {'positions':positions})
+        positions_list = Position.objects.order_by('id')  # Order by the 'id' field, you can change it to the desired field
+        paginator = Paginator(positions_list, 5)  # Show 5 positions per page
+
+        page_number = request.GET.get('page')
+        try:
+            positions = paginator.page(page_number)
+        except PageNotAnInteger:
+            positions = paginator.page(1)
+        except EmptyPage:
+            positions = paginator.page(paginator.num_pages)
+
+        return render(request, 'position.html', {'positions': positions})
     else:
         return redirect('login')
+
     
 # Here new position add
 def add_position(request):
@@ -33,6 +47,7 @@ def add_position(request):
         return redirect('position')
      else:
         return render(request, 'add_position.html', {'form':form}) 
+    
 
 
 # position edit here
@@ -53,14 +68,25 @@ def delete_position(request, id):
     return redirect('position')
 
 
+
 # all voter showing here
 def voters(request):
     if request.user.username == 'admin':
-      user = request.user
-      voters = User.objects.all() 
-      return render(request, 'voters.html', {'voters':voters})
+        voters_list = User.objects.order_by('id')  # Order by the 'id' field, you can change it to the desired field
+        paginator = Paginator(voters_list, 5)  # Show 5 voters per page
+
+        page_number = request.GET.get('page')
+        try:
+            voters = paginator.page(page_number)
+        except PageNotAnInteger:
+            voters = paginator.page(1)
+        except EmptyPage:
+            voters = paginator.page(paginator.num_pages)
+
+        return render(request, 'voters.html', {'voters': voters})
     else:
         return redirect('login')
+    
 
 # voter edit here
 def edit_voter(request, id):
@@ -89,12 +115,29 @@ def delete_voter(request, id):
   
 # showing all candidates here 
 def show_candidate(request):
-    if request.user.username == 'admin':
+     if request.user.username == 'admin':
       candidates = Candidate.objects.all() # all position assign to positons for showing
       return render(request, 'candidate.html', {'candidates':candidates})
-    else:
+     else:
         return redirect('login')
     
+    # if request.user.username == 'admin':
+    #     candidates_list = Candidate.objects.all()
+    #     paginator = Paginator(candidates_list, 10)  # Show 10 candidates per page
+
+    #     page_number = request.GET.get('page')
+    #     try:
+    #         candidates = paginator.page(page_number)
+    #     except PageNotAnInteger:
+    #         # If page is not an integer, default to the first page
+    #         candidates = paginator.page(1)
+    #     except EmptyPage:
+    #         # If page is out of range (e.g. 9999), deliver the last page of results
+    #         candidates = paginator.page(paginator.num_pages)
+
+    #     return render(request, 'candidate.html', {'candidates': candidates})
+    # else:
+    #     return redirect('login')
     
 # candidate add here for vote
 def create_candidate(request):
