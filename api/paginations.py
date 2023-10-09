79b3b8cd6f8c633668ea6aa7d31c249855e9d3c9
Md Ikhtiar Uddin <mdikhtiaruddin786@gmail.com>
2023-10-09 20:31:21 +0600

    backend pagination done

--- api/paginations.py
+++ api/paginations.py
@@ -1,2 +1,10 @@
 from rest_framework.pagination import PageNumberPagination
 
+class PositionPagination(PageNumberPagination):
+    page_size = 2
+class VotePagination(PageNumberPagination):
+    page_size = 2
+class VoterPagination(PageNumberPagination):
+    page_size = 2
+class CandidatePagination(PageNumberPagination):
+    page_size = 2
--- ducsu_online_voting/settings.py
+++ ducsu_online_voting/settings.py
@@ -67,16 +67,6 @@ WSGI_APPLICATION = 'ducsu_online_voting.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
