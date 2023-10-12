09f7be5bd155b1ff85c67277d6c69811be6524e8
Zayed-Rahat <azurahat129@gmail.com>
2023-10-12 20:15:50 +0600

    pagination-done

--- api/urls.py
+++ api/urls.py
@@ -1,25 +1,30 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from . import views
+from .views import *
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
-router.register(r'position', views.PositionList,basename="position")
-router.register(r'voter', views.VoterList,basename="voter")
-router.register(r'candidate', views.CandidateList,basename="candidate")
-router.register(r'vote', views.VoteList,basename="vote")
+router.register('position', PositionViewSet)
+router.register('voter', VoterViewSet)
+router.register('vote', VoterViewSet)
+router.register('candidate', CandidateViewSet)
+# router.register(r'position', views.PositionList,basename="position")
+# router.register(r'voter', views.VoterList,basename="voter")
+# router.register(r'candidate', views.CandidateList,basename="candidate")
+# router.register(r'vote', views.VoteList,basename="vote")
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
-    # path('position/', views.PositionListView.as_view()),
-    # path('position/<int:pk>/', views.PositionListUpdateDelete.as_view()),
-    path('position/', views.PositionList.as_view()),
-    path('position/<int:pk>/', views.PositionDeleteUpdate.as_view()),
-    path('voter/', views.VoterList.as_view()),
-    path('voter/<int:pk>/', views.VoterDeleteUpdate.as_view()),
-    path('candidate/', views.CandidateList.as_view()),
-    path('candidate/<int:pk>/', views.CandidateDeleteUpdate.as_view()),
-    path('vote/', views.VoteList.as_view()),
-    path('vote/<int:pk>/', views.VoteDeleteUpdate.as_view()),
+    path('', include(router.urls)),
+    # path('position/', PositionViewSet.as_view()),
+    # path('position/<int:pk>/', PositionDeleteUpdate.as_view()),
+    # path('position/', PositionList.as_view()),
+    # path('position/<int:pk>/', PositionDeleteUpdate.as_view()),
+    # path('voter/', VoterList.as_view()),
+    # path('voter/<int:pk>/', VoterDeleteUpdate.as_view()),
+    # path('candidate/', CandidateList.as_view()),
+    # path('candidate/<int:pk>/', CandidateDeleteUpdate.as_view()),
+    # path('vote/', VoteList.as_view()),
+    # path('vote/<int:pk>/', VoteDeleteUpdate.as_view()),
 ]
 
--- ducsu_online_voting/settings.py
+++ ducsu_online_voting/settings.py
@@ -67,16 +67,6 @@ WSGI_APPLICATION = 'ducsu_online_voting.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -87,7 +77,6 @@ DATABASES = {
         'PORT': '5432',          # Leave empty to use the default PostgreSQL port (usually 5432)
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
